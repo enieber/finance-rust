592f0a5835af077e2585b9ddecf51b08bd6b16d0
enieber cunha <eniebercunha@gmail.com>
2022-08-15 23:25:03 -0300

    feat: sum total values from csv

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,25 @@ use std::{
 
 /// # Read file as string
 /// receive path csv file in string and parse and print text with data parsed from file
-pub fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn read_file_line_by_line(filepath: &str) -> Result<f32, Box<dyn std::error::Error>> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
 
+    let mut total_value = 0.0;
+
     for line in reader.lines() {
         match line {
-            Ok(strLine) => {
-                let arrItems: Vec<&str> = strLine.split(",").collect();
-                let date = arrItems[0];
-                let value = arrItems[1];
-                let id = arrItems[2];
-                let description = arrItems[3];
-                println!(
-                    "{} in {} has valued: {}, with {}",
-                    id, date, value, description
-                );
+            Ok(str_line) => {
+                let arr_items: Vec<&str> = str_line.split(",").collect();
+                let value = arr_items[1];
+                let result_value: Result<f32, _> = value.parse();
+                match result_value {
+                    Ok(value_number) => total_value += value_number,
+                    Err(_) => {}
+                }
             }
-            Err(_) => println!("Deu ruim"),
+            Err(_) => {}
         }
     }
-
-    Ok(())
+    Ok(total_value)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use finance::read_file_line_by_line;
 
 fn main() {
-    read_file_line_by_line("test_data/header_file.csv");
+    let total_result = read_file_line_by_line("test_data/header_file.csv");
+    match total_result {
+        Ok(result) => println!("Total value of header_file is: {}", result),
+        Err(_) => println!("Not found total"),
+    }
 }
