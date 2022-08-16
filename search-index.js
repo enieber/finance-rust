b700a776cd3def07153eeb448f1f0ff0c31c08bd
enieber <enieber@users.noreply.github.com>
2022-08-16 02:25:53 +0000

    deploy: 592f0a5835af077e2585b9ddecf51b08bd6b16d0

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"finance":{"doc":"","t":[5],"n":["read_file_line_by_line"],"q":["finance"],"d":["Read file as string"],"i":[0],"f":[[[["str",0]],["result",4,[["box",3,[["error",8]]]]]]],"p":[]}\
+"finance":{"doc":"","t":[5],"n":["read_file_line_by_line"],"q":["finance"],"d":["Read file as string"],"i":[0],"f":[[[["str",0]],["result",4,[["f32",0],["box",3,[["error",8]]]]]]],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
