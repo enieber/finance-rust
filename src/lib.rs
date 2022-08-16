2f79f5a3220fcf651637383b0cba51e754c42df1
enieber cunha <eniebercunha@gmail.com>
2022-08-15 22:33:30 -0300

    feat: parse csv with get valued

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::{
+	  fs::{File},
+    io::{BufReader, BufRead}
+};
+use  std::result::Result::{Err, Ok};
+
+/// # Read file as string
+/// receive p in string and need implemenetation
+pub fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(strLine) => {
+                let arrItems: Vec<&str> = strLine.split(",").collect();
+                let date = arrItems[0];
+                let value = arrItems[1];
+                let id = arrItems[2];
+                let description = arrItems[3];
+                println!("{} in {} has valued: {}, with {}", id, date, value, description);
+            },
+            Err(_) => println!("Deu ruim"),
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,6 @@
-use std::{
-	error,
-	fs::{read_to_string, write},
-	path::Path,
-	result,
-};
-
-type TResult<T> = result::Result<T, TError>;
-type TError = Box<dyn error::Error>;
-
-
-/// # Read file as string
-/// receive p in string and need implemenetation
-fn read_file(p: &str) -> TResult<String> {
-	read_to_string(p).map_err(|e| e.into())
-}
+use finance::read_file_line_by_line;
 
 fn main() {
-    println!("Hello, world!");
+	read_file_line_by_line("test_data/header_file.csv");
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-    #[test]
-    fn test_read_file_with_success() {
-    	let result = read_file("test_data/empty_file.csv");
-    	assert!(result.is_ok());
-    }
-
-    #[test]
-    fn test_read_file_with_success_empty_file() {
-    	let result = read_file("test_data/empty_file.csv");
-
-    	if let Ok(s) = result {
-    		assert_eq!(s, "");
-    	}
-    }
-
-	#[test]
-    fn test_read_file_with_success_with_header() {
-    	let result = read_file("test_data/header_file.csv");
-
-    	if let Ok(s) = result {
- 	  		assert_eq!(s, "Data,Valor,Identificador,Descrição\n");    	
- 	  	}
-    }
-    
-}
