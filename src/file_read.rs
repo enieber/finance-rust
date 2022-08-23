ebb5ae2fa2972502d97a4fe9a9e080fbeb8d516e
enieber cunha <eniebercunha@gmail.com>
2022-08-22 23:58:49 -0300

    feat: create new file after process csv

--- src/file_read.rs
+++ src/file_read.rs
@@ -3,7 +3,6 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
-use std::io::prelude::*;
 
 /// # Read file as string
 /// receive path csv file in string and parse and calculate valus
@@ -12,13 +11,35 @@ pub fn read_file_and_sum_total(filepath: &str) -> Result<f32, Box<dyn std::error
     let reader = BufReader::new(file);
 
     let mut total_value = 0.0;
-    
-    for line in reader.lines() {
-        total_value += sum_total_by_line(line);
+    let lines = reader.lines();
+    let first_line_data = lines[1];
+    let line_object = split_line_to_vec(&line_object);
+    println!("test {}", line_object[0]);
+    let last_line = lines.last();
+    match last_line {
+        Some(some_line) => {
+            match some_line {
+                Ok(line_ok) => {
+                    let line_item = split_line_to_vec(&line_ok);
+                    println!("last lin is: {}", line_item[0]);
+                },
+                Err(_) => {}
+            }
+        },
+        None => {}
     }
+  //  for line in reader.lines() {
+    //    total_value += sum_total_by_line(line);
+    //}
     Ok(total_value)
 }
 
+fn split_line_to_vec(str_line: &String) -> Vec<&str> {
+    let list = str_line
+        .split(",")
+        .collect();
+    list
+}
 
 /// ## sum_total_by_line function sum item value in position 1
 /// example: date, value, id, description
@@ -26,7 +47,7 @@ fn sum_total_by_line(line: Result<String, std::io::Error>) -> f32 {
     let mut total_value = 0.0;
     match line {
             Ok(str_line) => {
-                let arr_items: Vec<&str> = str_line.split(",").collect();
+                let arr_items: Vec<&str> = split_line_to_vec(&str_line);
                 let value = arr_items[1];
                 let result_value: Result<f32, _> = value.parse();
                 match result_value {
@@ -38,11 +59,3 @@ fn sum_total_by_line(line: Result<String, std::io::Error>) -> f32 {
         }
     total_value
 }
-
-pub fn writter_file_sum(path_file: &str, sum: &str) -> Result<(),  Box<dyn std::error::Error>> {
-    let mut file_buffer = File::create(&path_file)?;
-    write!(file_buffer, "total sum\n");
-    write!(file_buffer, "{}\n", &sum);
-    Ok(())
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use finance::read_file_and_sum_total;
+use finance::writter_file_sum;
+
 
 fn main() {
     let total_result = read_file_and_sum_total("test_data/header_file.csv");
     match total_result {
-        Ok(result) => println!("Total value of header_file is: {}", result),
+        Ok(result) => {
+            println!("Total value of header_file is: {}", result);
+            writter_file_sum("test_data/total_header_file.csv", &result.to_string());
+        },
         Err(_) => println!("Not found total"),
     }
 }
